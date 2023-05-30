89be1724440b103eeb33049895611ab2ca22377d
rosiane <rosiane.avila@gmail.com>
2023-05-30 12:22:29 -0300

    Initial commit

---
+++ cypress/e2e/1-getting-started/alura-busca.cy.js
@@ -0,0 +1,14 @@
+describe('alura busca cursos', () => {
+
+    beforeEach(() => {
+        cy.visit('https://www.alura.com.br');
+    })
+
+    it('buscar curso de java', () => {
+        cy.get('#header-barraBusca-form-campoBusca').type('java');
+        cy.get('.header-barraBusca-form-submit').click();
+        cy.get('h4.busca-resultado-nome').should('contain', 'Formação Aprenda Java com Orientação a Objetos')
+
+    })
+
+})
